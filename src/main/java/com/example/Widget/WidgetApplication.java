3fca09d5ca4642779db41fc2a76839cdfce5a0d7
Deep <deeptemporary24@gmail.com>
2025-10-08 12:06:59 +0530

    initiated widget poc project

---
+++ src/main/java/com/example/Widget/WidgetApplication.java
@@ -0,0 +1,14 @@
+package com.example.Widget;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WidgetApplication {
+
+	public static void main(String[] args) {
+        SpringApplication.run(WidgetApplication.class, args);
+		System.out.println("Hello World");
+	}
+
+}
