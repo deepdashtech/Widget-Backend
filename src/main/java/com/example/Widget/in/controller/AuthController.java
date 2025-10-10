87a50cbf54b993dd00e4135cfc3e4bcdf2cc2e07
Krishnang Thakkar <krishnang.t@dashtechinc.com>
2025-10-10 15:16:15 +0530

    Added JWT Token and Spring Security

--- src/main/java/com/example/Widget/in/controller/AuthController.java
+++ src/main/java/com/example/Widget/in/controller/AuthController.java
@@ -1,5 +1,6 @@
 package com.example.Widget.in.controller;
 
+import com.example.Widget.in.config.JwtTokenUtil;
 import com.example.Widget.in.dto.ApiResponse;
 import com.example.Widget.in.dto.LoginRequestDto;
 import com.example.Widget.in.entities.user;
@@ -14,17 +15,24 @@ import com.example.Widget.in.dto.userResponse;
 @RestController
 @RequestMapping("/api/auth")
 @CrossOrigin(origins = "http://localhost:5173")
-public class AuthController {
+public class AuthController
+{
 
     @Autowired
     private UserService userService;
 
+    @Autowired
+    private JwtTokenUtil jwtTokenUtil;
+
+
 
     @PostMapping("/register")
-    public ResponseEntity<ApiResponse<user>> registerUser(@RequestBody LoginRequestDto request) {
+    public ResponseEntity<ApiResponse<user>> registerUser(@RequestBody LoginRequestDto request)
+    {
         user userResponse = userService.RegisterUser(request.getUsername(), request.getPassword());
 
-        if (userResponse != null) {
+        if (userResponse != null)
+        {
             ApiResponse<user> res = new ApiResponse<>(true, "Successfully Registered", userResponse);
             return ResponseEntity.ok(res);
         }
@@ -33,16 +41,23 @@ public class AuthController {
         return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(res);
     }
 
+
+
     @PostMapping("/login")
-    public  ResponseEntity<ApiResponse<user>> loginUser(@RequestBody LoginRequestDto user)
+    public  ResponseEntity<ApiResponse<String>> loginUser(@RequestBody LoginRequestDto user)
     {
         user userResponse=userService.LoginUser(user.getUsername(),user.getPassword());
-        if (userResponse != null) {
-            ApiResponse<user> res = new ApiResponse<>(true, "Successfully Logged", userResponse);
+
+        if (userResponse != null)
+        {
+
+            String token = jwtTokenUtil.generateToken(userResponse.getUsername());
+
+            ApiResponse<String> res = new ApiResponse<>(true, "Successfully Logged",token);
             return ResponseEntity.ok(res);
         }
 
-        ApiResponse<user> res = new ApiResponse<>(false, "Failed To Login", null);
+        ApiResponse<String> res = new ApiResponse<>(false, "Failed To Login", null);
         return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(res);
     }
 }
