00929a7a8f763311bb3e931225d59ba16385162e
Krishnang Thakkar <krishnang.t@dashtechinc.com>
2025-10-15 10:24:08 +0530

    added JwtAuthenticationEntryPoint

--- src/main/java/com/example/Widget/in/controller/WidgetConroller.java
+++ src/main/java/com/example/Widget/in/controller/WidgetConroller.java
@@ -15,6 +15,7 @@ import org.springframework.web.multipart.MultipartFile;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
+import java.util.List;
 
 
 @RestController
@@ -25,63 +26,73 @@ public class WidgetConroller {
     @Autowired
     private WidgetService widgetService;
 
-    @PostMapping(value = "/add", consumes = MediaType.MULTIPART_FORM_DATA_VALUE)
-    public ResponseEntity<ApiResponse<widget>> addWidget(@RequestPart("widget") WidgetRequest widgetRequest, @RequestPart("icon")MultipartFile iconFile) {
 
-        String fileName = null;
-        try {
-            // Directory for storing uploaded widget icons
-            String uploadDir = "uploads/widgets/";
-            Path uploadPath = Paths.get(uploadDir).toAbsolutePath().normalize();
 
-            if (!Files.exists(uploadPath)) {
-                Files.createDirectories(uploadPath);
-            }
+    @GetMapping("/allwidgets")
+    public ResponseEntity<ApiResponse<List<widget>>> getAllWidget()
+    {
 
-            // Unique filename based on widget code or name
-            String originalFilename = iconFile.getOriginalFilename();
-            String extension = originalFilename.substring(originalFilename.lastIndexOf("."));
-            fileName = "Widget_" + widgetRequest.getTitle() + extension;
-
-            Path filePath = uploadPath.resolve(fileName);
-            iconFile.transferTo(filePath.toFile());
-
-        } catch (Exception e) {
-            e.printStackTrace();
-            ApiResponse<widget> res = new ApiResponse<>(
-                    false,
-                    "Failed to upload icon: " + e.getMessage(),
-                    null
-            );
-//            logger.error("Failed to upload widget icon.");
-            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(res);
-        }
-
-        boolean added = widgetService.AddWidget(
-                widgetRequest.getTitle(),
-                widgetRequest.getDescription(),
-                "/uploads/widgets/" + fileName,
-                widgetRequest.getDefaultHeight(),
-                widgetRequest.getDefaultWidth()
-        );
-
-        if (added) {
-            ApiResponse<widget> res = new ApiResponse<>(
-                    true,
-                    "Widget added successfully",
-                    null
-            );
-//            logger.info("Widget data saved successfully.");
-            return ResponseEntity.ok(res);
-        }
-
-        ApiResponse<widget> res = new ApiResponse<>(
-                false,
-                "Widget already exists",
-                null
-        );
-//        logger.warn("Widget creation conflict - already exists.");
-        return ResponseEntity.status(HttpStatus.CONFLICT).body(res);
-
+        return ResponseEntity.ok(widgetService.listWidgets());
     }
+
+
+//    @PostMapping(value = "/add", consumes = MediaType.MULTIPART_FORM_DATA_VALUE)
+//    public ResponseEntity<ApiResponse<widget>> addWidget(@RequestPart("widget") WidgetRequest widgetRequest, @RequestPart("icon")MultipartFile iconFile) {
+//
+//        String fileName = null;
+//        try {
+//            // Directory for storing uploaded widget icons
+//            String uploadDir = "uploads/widgets/";
+//            Path uploadPath = Paths.get(uploadDir).toAbsolutePath().normalize();
+//
+//            if (!Files.exists(uploadPath)) {
+//                Files.createDirectories(uploadPath);
+//            }
+//
+//            // Unique filename based on widget code or name
+//            String originalFilename = iconFile.getOriginalFilename();
+//            String extension = originalFilename.substring(originalFilename.lastIndexOf("."));
+//            fileName = "Widget_" + widgetRequest.getTitle() + extension;
+//
+//            Path filePath = uploadPath.resolve(fileName);
+//            iconFile.transferTo(filePath.toFile());
+//
+//        } catch (Exception e) {
+//            e.printStackTrace();
+//            ApiResponse<widget> res = new ApiResponse<>(
+//                    false,
+//                    "Failed to upload icon: " + e.getMessage(),
+//                    null
+//            );
+////            logger.error("Failed to upload widget icon.");
+//            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(res);
+//        }
+//
+//        boolean added = widgetService.AddWidget(
+//                widgetRequest.getTitle(),
+//                widgetRequest.getDescription(),
+//                "/uploads/widgets/" + fileName,
+//                widgetRequest.getDefaultHeight(),
+//                widgetRequest.getDefaultWidth()
+//        );
+//
+//        if (added) {
+//            ApiResponse<widget> res = new ApiResponse<>(
+//                    true,
+//                    "Widget added successfully",
+//                    null
+//            );
+////            logger.info("Widget data saved successfully.");
+//            return ResponseEntity.ok(res);
+//        }
+//
+//        ApiResponse<widget> res = new ApiResponse<>(
+//                false,
+//                "Widget already exists",
+//                null
+//        );
+////        logger.warn("Widget creation conflict - already exists.");
+//        return ResponseEntity.status(HttpStatus.CONFLICT).body(res);
+//
+//    }
 }
