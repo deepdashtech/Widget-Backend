568476679a3eb6be5431f69b81e18b9c3fae4ebb
Deep <deeptemporary24@gmail.com>
2025-10-16 18:49:53 +0530

    Added All 5 APIs

--- src/main/java/com/example/Widget/in/dto/ApiResponse.java
+++ src/main/java/com/example/Widget/in/dto/ApiResponse.java
@@ -2,12 +2,14 @@ package com.example.Widget.in.dto;
 
 
 import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
+@Builder
 public class ApiResponse<T>
 {
     private boolean success;
